778c65fb6de29dfed193203d85c4f2e21af48e4e
Utkarsh Patel <utkarshjp7@gmail.com>
2018-06-17 16:39:51 -0400

    Update Stack.java

--- General/Stack.java
+++ General/Stack.java
@@ -2,14 +2,15 @@ public class Stack {
 
     public static class StackNode<T> {
         public T data;
-        public StackNode next;
+        public StackNode<T> next;
+        public T minOfSubstack;
         
         public StackNode(T data) {
             this.data = data;
         }
     }
     
-    public static class Stack<T> {
+    public static class Stack<T extends Comparable<T>> {
         
         public StackNode<T> top;
         
@@ -17,6 +18,13 @@ public class Stack {
             StackNode<T> node = new StackNode<>(data);
             node.next = top;
             top = node;
+            
+            if (top.next != null) {
+                top.minOfSubstack = (top.data.compareTo(top.next.minOfSubstack) < 0) ? top.data : top.next.minOfSubstack;
+            }
+            else {
+                top.minOfSubstack = top.data;
+            }
         }
         
         public T pop() {
@@ -34,15 +42,30 @@ public class Stack {
         public boolean isEmpty() {
             return top == null;
         }
+        
+        public T min() {
+            if (top != null)
+                return top.minOfSubstack;
+            else
+                return null;
+        }
     }
 
      public static void main(String []args){
         Stack<Integer> stack = new Stack<>();
         
         stack.push(10);
+        System.out.println(stack.min());
         stack.push(20);
+        System.out.println(stack.min());
         stack.push(30);
+        System.out.println(stack.min());
+        stack.push(5);
+        System.out.println(stack.min());
+        stack.push(6);
+        
         stack.pop();
-        System.out.println(stack.pop());
+        stack.pop();
+        System.out.println(stack.min());
      }
 }
